c51d093fef31dc648f40983c014dbdd373a270fa
Riikka Alanen <69243343+AlanenR@users.noreply.github.com>
2022-06-05 13:00:21 -0500

    JavaScript/JQuery practice - Simon game
    Added starting files, functions for animations + sounds, and event listeners for button clicks + keypress.

---
+++ Simon-Game/game.js
@@ -0,0 +1,51 @@
+var btnColors = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+var userClickedPattern = [];
+
+// Button event handeler to detect clicks and add the id to UserClicked pattern array
+var btnClicked = $(".btn").on("click", function(event){
+    var userChosenColor = (event.target.id);
+    userClickedPattern.push(userChosenColor);
+    addSound(userChosenColor);
+    animatePress(userChosenColor);
+});
+
+// Detect first keypress and start game
+var firstPress = $(document).keypress(function(){
+
+});
+
+
+
+
+function nextSequence() {
+    var randomNbr = Math.floor(Math.random() * 4);
+    var randomChosenColor = btnColors[randomNbr];
+    gamePattern.push(randomChosenColor);
+    addAnimation(randomChosenColor);
+    addSound(randomChosenColor);
+}
+
+
+// Flash animation for the random button
+function addAnimation(id) {
+    $("#" + id).fadeOut(100).fadeIn(100);
+
+}
+
+// Plays sound for the random button and clicked button
+function addSound(sound){
+    var audio = new Audio('sounds/' + sound + '.mp3');
+    audio.play();
+}
+
+function animatePress(currentColor){
+    $("." + currentColor).addClass("pressed");
+    setTimeout(function(){
+        $("." + currentColor).removeClass("pressed");
+    }), 100;
+
+}
+
+
+
