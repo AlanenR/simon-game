fad21c2e1ed8095062b3d277630b3e105e38afef
Riikka Alanen <69243343+AlanenR@users.noreply.github.com>
2022-06-06 10:19:13 -0500

    Added answer checks and modified first keypress

--- Simon-Game/game.js
+++ Simon-Game/game.js
@@ -1,11 +1,21 @@
 var btnColors = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userClickedPattern = [];
+var gameOn = false;
+var level = 0;
 
 // Detect first keypress and start game
-$(document).one("keypress", function(){
-    nextSequence();
+$(document).keypress(function(){
+    if(!gameOn) {
+        gameOn = true;
+        nextSequence();
+    }
 });
+
+// Updates H1 according to the level
+function updateH1(level){
+        $("#level-title").text("Level " + level);
+}
 
 // Button event handeler to detect clicks and add the id to UserClicked pattern array
 var btnClicked = $(".btn").on("click", function(event){
@@ -13,14 +23,32 @@ var btnClicked = $(".btn").on("click", function(event){
     userClickedPattern.push(userChosenColor);
     addSound(userChosenColor);
     animatePress(userChosenColor);
+    checkAnswer(level);
 });
 
+// Machine game pattern
 function nextSequence() {
-    var randomNbr = Math.floor(Math.random() * 4);
-    var randomChosenColor = btnColors[randomNbr];
-    gamePattern.push(randomChosenColor);
-    addAnimation(randomChosenColor);
-    addSound(randomChosenColor);
+            level++;
+            updateH1(level);
+            var randomNbr = Math.floor(Math.random() * 4);
+            var randomChosenColor = btnColors[randomNbr];
+            gamePattern.push(randomChosenColor);
+            addAnimation(randomChosenColor);
+            addSound(randomChosenColor);
+}
+
+// Checks user clicks against the game pattern
+function checkAnswer(level){
+    var machine = gamePattern[level - 1];
+    var player = userClickedPattern[level - 1]
+    if(machine === player) {
+        nextSequence();
+    }
+    else if(machine != player) {
+        alert("loser");
+    }
+    console.log(machine);
+    console.log(player);
 }
 
 
@@ -37,9 +65,9 @@ function addSound(sound){
 }
 
 function animatePress(currentColor){
-    $("." + currentColor).addClass("pressed");
+    $("#" + currentColor).addClass("pressed");
     setTimeout(function(){
-        $("." + currentColor).removeClass("pressed");
+        $("#" + currentColor).removeClass("pressed");
     }), 100;
 
 }
